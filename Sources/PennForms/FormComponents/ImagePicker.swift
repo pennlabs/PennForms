17ce1f9a60fd0c4d1a355f375bf03662d3e3fbfc
JHawk0224 <jhawk111@icloud.com>
2024-02-29 07:48:31 -0500

    Refactor ImagePicker

--- Sources/PennForms/FormComponents/ImagePicker.swift
+++ Sources/PennForms/FormComponents/ImagePicker.swift
@@ -9,80 +9,59 @@ import SwiftUI
 import PhotosUI
 
 struct ImagePicker: View {
-    
-    @State var maxSelection: [PhotosPickerItem] = []
-    @State var selectedImages: [UIImage] = []
+    @State var selection: [PhotosPickerItem] = []
+    @State var selectedImages: [Image] = []
     @State var maxSelectionCount: Int = 5
     
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $maxSelection,
+        VStack(alignment: .leading, spacing: 10) {
+            PhotosPicker(selection: $selection,
                          maxSelectionCount: maxSelectionCount,
                          matching: .any(of: [.images, .not(.videos)])) {
-                VStack{
+                VStack(spacing: 8) {
                     Image(systemName: "photo.badge.plus")
-                        .padding(3)
                     Text("Add Photos")
                 }
-                        .frame(width: 350, height: 200)
-                        .background(Rectangle()
-                            .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [7]))
-                            .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                        .foregroundColor(Color.gray)
-                        .padding()
+                .frame(width: 350, height: 200)
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [7])))
+                .foregroundColor(Color.secondary)
             }
-            .onChange(of: maxSelection) { newValue in
+            .onChange(of: selection) { newSelection in
                 Task {
-                    selectedImages = []
-                    for value in newValue {
-                        if let imageData = try? await
-                            value.loadTransferable(type: Data.self), let image = UIImage(data: imageData) {
+                    selectedImages.removeAll()
+                    for item in newSelection {
+                        if let image = try? await item.loadTransferable(type: Image.self) {
                             selectedImages.append(image)
                         }
                     }
                 }
             }
-            .padding(-25)
             
-            if selectedImages.count > 0 {
-                ScrollView(.horizontal) {
-                    HStack {
-                        if selectedImages.count < maxSelectionCount {
-                            VStack{
-                                Image(systemName: "photo.badge.plus")
-                                    .padding(3)
-                                Text("Add Photos")
-                                    
-                            }
-                                    .frame(width: 120, height: 120)
-                                    .background(Rectangle()
-                                        .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [7]))
-                                        .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                                    .foregroundColor(Color.gray)
-                                    .padding()
-                        }
-                        ForEach(selectedImages, id: \.self)
-                        {img in Image(uiImage: img)
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    if selectedImages.count > 0 {
+                        ForEach(Array(selectedImages.enumerated()), id: \.offset) { index, image in
+                            image
                                 .resizable()
-                                .scaledToFill()
+                                .aspectRatio(contentMode: .fit)
                                 .frame(width: 120, height: 120)
-                                .clipShape(Rectangle())
-                                
-                            
                         }
-                        ForEach(0..., maxSelectionCount - 1 - selectedImages.count) {
-                                Image(systemName: "photo.badge.plus")
-                                    .padding(3)
-                                    .frame(width: 120, height: 120)
-                                    .background(Rectangle()
-                                        .strokeBorder(style: StrokeStyle(lineWidth: 1))
-                                        .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-                                    .foregroundColor(Color.gray)
-                                    .padding()
+                    }
+                    if selectedImages.count < maxSelectionCount {
+                        ForEach(0..<(maxSelectionCount - selectedImages.count), id: \.self) { _ in
+                            Image(systemName: "photo.badge.plus")
+                                .frame(width: 120, height: 120)
+                                .background(RoundedRectangle(cornerRadius: 8)
+                                    .strokeBorder(style: StrokeStyle(lineWidth: 0.5)))
+                                .foregroundColor(Color.secondary)
                         }
                     }
                 }
             }
+            Text("Add up to \(maxSelectionCount) photo\(maxSelectionCount == 1 ? "" : "s")")
+                .font(.subheadline)
+                .foregroundColor(Color.secondary)
         }
     }
 }
