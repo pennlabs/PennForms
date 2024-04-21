6b6b4593934f89f7d0c98ab4580e3dbd0f8dd7e6
christinaqiu3 <christinaqiu3@gmail.com>
2024-04-21 14:02:01 -0400

    fixed datepicker to allow for selection between dates and properly cutoff dates before the present

--- Sources/PennForms/FormComponents/DateRangeField.swift
+++ Sources/PennForms/FormComponents/DateRangeField.swift
@@ -4,7 +4,8 @@ public struct DateRangeField: FormComponent {
     
     @Binding var lowerDate: Date
     @Binding var upperDate: Date
-    @State var wasSet: Bool = false
+    @State var wasSet1: Bool = false
+    @State var wasSet2: Bool = false
     @Environment(\.validator) var validator
     @Environment(\.showValidationErrors) var showValidationErrors
     
@@ -48,8 +49,8 @@ public struct DateRangeField: FormComponent {
                     .bold()
             }
             HStack {
-                DateRangeSubfield(date: $lowerDate, range: self.range.lowerBound...upperDate, placeholder: lowerPlaceholder, wasSet: $wasSet)
-                DateRangeSubfield(date: $upperDate, range: lowerDate...self.range.upperBound, placeholder: upperPlaceholder, wasSet: $wasSet)
+                DateRangeSubfield(date: $lowerDate, range: self.range.lowerBound...upperDate, placeholder: lowerPlaceholder, wasSet1: $wasSet1, wasSet2: $wasSet2, set: 1)
+                DateRangeSubfield(date: $upperDate, range: self.range.lowerBound...self.range.upperBound, placeholder: upperPlaceholder, wasSet1: $wasSet1, wasSet2: $wasSet2, set: 2)
             }
             
             if showValidationErrors, !validator.isValid(lowerDate as AnyValidator.Input) || !validator.isValid(upperDate as AnyValidator.Input), let validatorMessage = validator.message {
@@ -62,8 +63,10 @@ public struct DateRangeField: FormComponent {
             }
         }
         .padding(.bottom, 5)
-        .onChange(of: wasSet) { _ in
+        .onChange(of: wasSet1) { _ in
             self.lowerDate = self.range.lowerBound == .distantPast ? .now : self.range.lowerBound
+        }
+        .onChange(of: wasSet2) { _ in
             self.upperDate = Calendar.current.date(byAdding: .day, value: upperOffset, to: .now) ?? (self.range.upperBound == .distantFuture ? .now : self.range.upperBound)
         }
     }
@@ -73,7 +76,9 @@ private struct DateRangeSubfield: View {
     @Binding var date: Date
     var range: ClosedRange<Date>
     var placeholder: String?
-    @Binding var wasSet: Bool
+    @Binding var wasSet1: Bool
+    @Binding var wasSet2: Bool
+    var set: Int
     
     @Environment(\.validator) var validator
     @Environment(\.colorScheme) var colorScheme
@@ -83,7 +88,11 @@ private struct DateRangeSubfield: View {
     var body: some View {
         Button {
             isPickerVisible.toggle()
-            wasSet = true
+            if set == 1 {
+                    wasSet1 = true
+            } else {
+                    wasSet2 = true
+            }
         } label: {
             HStack {
                 Group {
